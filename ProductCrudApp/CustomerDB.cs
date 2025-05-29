2ce618af5ee70117a85f5f0af241b7e19be4f1e9
Logan Nelson <Lnelson0915@gmail.com>
2025-05-29 10:44:13 -0700

    Add sql file for ProductDB

---
+++ ProductCrudApp/CustomerDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCrudApp
+{
+    internal class CustomerDB
+    {
+        public List<string> GetAllCustomers()
+        {
+            return null;
+        }
+    }
+}
