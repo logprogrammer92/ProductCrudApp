3f11e25536f632ea9e02e1fdf54ba05b84cb8901
Logan Nelson <Lnelson0915@gmail.com>
2025-06-03 11:22:36 -0700

    Add a Product class to support CRUD funcitonallity for salable products

---
+++ ProductCrudApp/Product.cs
@@ -0,0 +1,27 @@
+﻿
+namespace ProductCrudApp;
+
+/// <summary>
+/// Represents a single product for sale
+/// </summary>
+public class Product
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the product. The
+    /// database will generate this value for new products. Leave
+    /// at 0 if this is new
+    /// </summary>
+    public int ProductId {  get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the product. Acceptable characters
+    /// are A-Z (case insensitive) and digits. Can be up to 150 characters
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the sales price of the product. Price must be greater than 0
+    /// </summary>
+    public double SalesPrice { get; set; }
+
+}
